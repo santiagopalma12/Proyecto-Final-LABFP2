6a10fa04c7dfe6e3227a2139ecb48b85a77d1457
BacaCalsinLeonardo <lbacac@unsa.edu.pe>
2024-12-12 20:29:22 -0500

    Update Entrenador.java

--- Entrenador.java
+++ Entrenador.java
@@ -7,13 +7,34 @@ public class Entrenador implements Serializable {
     private String nombre;
     private List<Criatura> equipo;
     private List<Criatura> coleccion;
+    private int pocionesCuracion;
 
     public Entrenador(String nombre) {
         this.nombre = nombre;
         this.equipo = new ArrayList<>();
         this.coleccion = new ArrayList<>();
+        this.pocionesCuracion = 4; // Por ejemplo, 4 pociones iniciales
+
+    }
+    public void restarPocionesCuracion(int cantidad) {
+        if (cantidad > 0 && pocionesCuracion >= cantidad) {
+            pocionesCuracion -= cantidad;
+        }
     }
 
+    public boolean usarPocionCuracion(Criatura criatura, int cantidad) {
+        if (pocionesCuracion >= cantidad) {
+            int totalCuracion = cantidad * 20;
+            criatura.curar(totalCuracion);
+            pocionesCuracion -= cantidad;
+            System.out.println(nombre + " usó " + cantidad + " pociones de curación. Pociones restantes: " + pocionesCuracion);
+            return true;
+        } else {
+            System.out.println("No tienes suficientes pociones de curación.");
+            return false;
+        }
+    }
+    
     public void capturarCriatura(Criatura criatura) {
         if (equipo.size() < 3) {
             equipo.add(criatura);
@@ -34,11 +55,14 @@ public class Entrenador implements Serializable {
         return equipo;
     }
 
-
     public String getNombre() {
         return nombre;
     }
     public List<Criatura> getColeccion() {
         return coleccion;
     }
+        // Getters para pociones
+    public int getPocionesCuracion() {
+         return pocionesCuracion;
+    }   
 }
